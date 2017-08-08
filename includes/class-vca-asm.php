4e036eb497bf458df8723802bc65460bb69fd575
Tobias Kaestle <t.kaestle@vivaconagua.org>
2017-08-09 00:35:26 +0200

    hotfix for session folder

--- includes/class-vca-asm.php
+++ includes/class-vca-asm.php
@@ -355,6 +355,7 @@ class VCA_ASM
 	public function start_session()
 	{
 		if( ! session_id() ) {
+			ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../sessions'));
 			session_start();
 		}
 	}
