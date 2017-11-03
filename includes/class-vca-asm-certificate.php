63bbbfc3f64a1fb19ecb2fca98064477756dc3b8
Tobias Kaestle <t.kaestle@vivaconagua.org>
2017-11-03 15:46:05 +0100

    added text to wordpress and adjusted new positions

--- includes/class-vca-asm-certificate.php
+++ includes/class-vca-asm-certificate.php
@@ -17,27 +17,22 @@ class VcA_ASM_Certificate
 
     private $regions;
 
-    private $language_templates = array(
+    private $template_informations = array(
         'de' => array(
             'date_format' => 'd.m.Y',
-            'output_filename' => 'viva_con_agua_ehrenamtsbestaetigung',
-            'registration' => array(
-                'x' => 70, 'y' => 86.5
-            ),'date' => array(
-                'x' => 41.1, 'y' => 177.3
-            ),'thankyou' => array(
-                'x' => 161.7, 'y' => 176, 'rotation' => 10
-            )
+            'output_filename' => 'viva_con_agua_ehrenamtsbestaetigung'
         ),
         'en' => array(
             'date_format' => 'm-d-Y',
-            'output_filename' => 'viva_con_agua_volunteer_certificate',
+            'output_filename' => 'viva_con_agua_volunteer_certificate'
+        ),
+        'positions' => array(
             'registration' => array(
-                'x' => 121, 'y' => 86.5
+                'x' => 0, 'y' => 86.5
             ),'date' => array(
-                'x' => 41.1, 'y' => 177.3
+                'x' => 20, 'y' => 177.3
             ),'thankyou' => array(
-                'x' => 161.7, 'y' => 176, 'rotation' => 10
+                'x' => 156, 'y' => 168, 'rotation' => 10
             )
         )
     );
@@ -81,9 +76,7 @@ class VcA_ASM_Certificate
 
         $pdf->AddPage();
 
-        //$lang = 'en';
-
-        $pdf->setSourceFile(VCA_ASM_ABSPATH . '/pdf-templates/volunteer_certificate_' . $lang . '.pdf');
+        $pdf->setSourceFile(VCA_ASM_ABSPATH . '/pdf-templates/volunteer_certificate.pdf');
 
         $tplIdx = $pdf->importPage(1);
         $pdf->useTemplate($tplIdx, 0, 0, 0, 0, true);
@@ -96,38 +89,44 @@ class VcA_ASM_Certificate
         $pdf->SetY(22);
         $pdf->Cell(0, 20, utf8_decode($this->user->first_name . " " . $this->user->last_name), 0, 1, 'C');
 
-        // Write date of registration
+        // Write content
+
+        $textContent = __("is a voluntary supporter of Viva con Agua since %since% in our regional group in %city%.\n\nViva con Agua is a network of people and organisations committed to\nestablish access to clean drinking water and basic sanitation for all humans\nworldwide.\n\nThere are thousands of voluntary supporters which are using creative and\njoyful activities to raise awareness for the global issues WATER, SANITATION\nand HYGIENE (short WASH) and simultaneously raise funds for our water\nprojects.", 'vca-asm');
+
+        $textContent = str_replace('%city%', $this->regions[$this->user->city], $textContent);
+        $textContent = str_replace('%since%', $user_registration, $textContent);
 
         $pdf->SetFont('MuseoSans300', '', '13');
         $pdf->SetTextColor(0,0,0);
 
-        $registration_position = $this->language_templates[$lang]['registration'];
+        $registration_position = $this->template_informations['positions']['registration'];
         $pdf->SetXY($registration_position['x'], $registration_position['y']);
-        $pdf->Write(9, $user_registration);
-
-        // Write active city
-
-        $pdf->SetX(0);
-        $pdf->Cell(0, 20, utf8_decode($this->regions[$this->user->city]) . '.', 0, 1, 'C');
+        $pdf->MultiCell(0, 5, utf8_decode($textContent), 0, 'C');
 
         // Write date of creation
 
+        $textCity = __("Hamburg, %date%\n\n\nMario Dresing, Coordination and supervision of voluntary", 'vca-asm');
+        $textCity = str_replace('%date%', date($this->template_informations[$lang]['date_format']), $textCity);
+
         $pdf->SetFont('MuseoSans300', '', '12');
 
-        $date_position = $this->language_templates[$lang]['date'];
+        $date_position = $this->template_informations['positions']['date'];
         $pdf->SetXY($date_position['x'], $date_position['y']);
-        $pdf->Write(0, date($this->language_templates[$lang]['date_format']));
+        $pdf->MultiCell(0, 5, utf8_decode($textCity));
 
         // Write Thanks
 
+        $textThanks = __("Thanks\n%name%", 'vca-asm');
+        $textThanks = str_replace('%name%', $this->user->first_name, $textThanks);
+
         $pdf->SetFont('MuseoSans500', '', '11');
 
-        $thankyou_position = $this->language_templates[$lang]['thankyou'];
+        $thankyou_position = $this->template_informations['positions']['thankyou'];
         $pdf->SetXY($thankyou_position['x'], $thankyou_position['y']);
         $pdf->Rotate($thankyou_position['rotation']);
-        $pdf->Write(0, $this->user->first_name);
+        $pdf->MultiCell(22, 5, utf8_decode($textThanks), 0, 'C');
 
-        $pdf->Output($this->language_templates[$lang]['output_filename'] . '.pdf', 'D');
+        $pdf->Output($this->template_informations[$lang]['output_filename'] . '.pdf', 'D');
 
     }
 
@@ -136,14 +135,15 @@ class VcA_ASM_Certificate
 
         $user_registration = strtotime($this->user->user_registered);
         $registration_year = date('Y', $user_registration);
+        $registration_month = date('m', $user_registration);
 
         switch ($lang) {
             case 'de':
-                $registration_string = $registration_year;
+                $registration_string = $registration_month . '.' . $registration_year;
                 break;
             case 'en':
             default:
-                $registration_string = $registration_year;
+                $registration_string = $registration_year . '-' . $registration_month;
                 break;
         }
 
