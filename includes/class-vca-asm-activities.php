d6b7002296e24588324e061af537c798baaea568
Tobias Kaestle <t.kaestle@vivaconagua.org>
2019-01-08 15:04:03 +0100

    changed email sender

--- includes/class-vca-asm-activities.php
+++ includes/class-vca-asm-activities.php
@@ -1663,7 +1663,7 @@ class VCA_ASM_Activities
 			$author_city_id = get_user_meta($post->post_author, 'city', true);
 			$author_city_id = empty($author_city_id) ? $post->post_author : $author_city_id;
 			
-			var_dump($author_city_id);
+			var_dump('1 - ' . $author_city_id);
 			
 			$author_city_user_id = $wpdb->get_var(
 					"SELECT user_id FROM " .
@@ -1696,6 +1696,8 @@ class VCA_ASM_Activities
 			$author_city_id = get_user_meta($post->post_author, 'city', true);
 			$author_city_id = empty($author_city_id) ? $post->post_author : $author_city_id;
 			
+			var_dump('2 - ' . $author_city_id);
+			
 			$author_city_user_id = $wpdb->get_var(
 					"SELECT user_id FROM " .
 					$wpdb->prefix . "vca_asm_geography " .
@@ -1726,6 +1728,8 @@ class VCA_ASM_Activities
 			
 			$author_city_id = get_user_meta($post->post_author, 'city', true);
 			$author_city_id = empty($author_city_id) ? $post->post_author : $author_city_id;
+			
+			var_dump('3 - ' . $author_city_id);
 			
 			$author_city_user_id = $wpdb->get_var(
 					"SELECT user_id FROM " .
